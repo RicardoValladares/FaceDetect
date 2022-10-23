f642b55640902283690b46fa1daa64a0abd360e1
RicardoValladares <r_a_v_r_@hotmail.com>
2022-10-23 11:39:40 -0600

    auto upload
    unificando auto upload

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response
 import time
 
+PATH_TO_TEST_IMAGES_DIR = './upload'
 app = Flask(__name__)
 
 @app.route('/')
@@ -27,6 +28,12 @@ def frontalfacexml():
 def eyexml():
     return Response(open('./web/haarcascade_eye.xml').read(), mimetype="application/xml")
 
+@app.route('/upload', methods=['POST'])
+def image():
+    i = request.files['image']
+    f = ('%s.jpeg' % time.strftime("%Y%m%d-%H%M%S"))
+    i.save('%s/%s' % (PATH_TO_TEST_IMAGES_DIR, f))
+    return Response("%s saved" % f)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

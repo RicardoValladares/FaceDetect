3c814188ac9c94bb02e1c65a0493cc5adf8958e4
RicardoValladares <r_a_v_r_@hotmail.com>
2022-11-17 19:40:01 -0600

    Identificar & Enrrolar

--- main.go
+++ main.go
@@ -1,43 +1,52 @@
 package main
 
 import (
-	"io/ioutil"
+	//"io/ioutil"
 	"net/http"
 	"fmt"
+	"os"
+	"io"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/index.html")})
-	http.HandleFunc("/index.html", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/index.html")})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/identificar.html")})
+	http.HandleFunc("/enrrolar.html", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/enrrolar.html")})
 	http.HandleFunc("/opencv.js", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/opencv.js")})
 	http.HandleFunc("/utils.js", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/utils.js")})
 	http.HandleFunc("/haarcascade_frontalface_default.xml", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/haarcascade_frontalface_default.xml")})
-	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) { 
+	http.Handle("/enrrolados/", http.StripPrefix("/enrrolados", (http.FileServer(http.Dir("./enrrolados"))) ))
+	http.HandleFunc("/enrrolar", func(w http.ResponseWriter, r *http.Request) { 
+		identificador := r.FormValue("id")
+		if identificador == "" || identificador == "null" {
+			http.Error(w, "Error al obtener el identificador", http.StatusBadRequest)
+			return
+		}
 		file, _, err := r.FormFile("image")
 		if err != nil {
-			http.Error(w, "Error al obtener el archivo el servidor", http.StatusBadRequest)
+			http.Error(w, "Error al obtener el imagen", http.StatusBadRequest)
 			file.Close()
 			return
 		}
 		defer file.Close()
-		tempFile, err := ioutil.TempFile("upload", "*"+".png" )
+		dst, err := os.Create("./enrrolados/"+identificador)
 		if err != nil {
-			http.Error(w, "La carpeta subidos no Existe o No se logro ramdomizar el nombre", http.StatusBadRequest)
+			http.Error(w, "Error al enrrolar imagen", http.StatusBadRequest)
 			file.Close()
 			return
 		}
-		defer tempFile.Close()
-		fileBytes, err := ioutil.ReadAll(file)
+		defer dst.Close()
+		_, err = io.Copy(dst, file)
 		if err != nil {
-			http.Error(w, "No se logro leer el archivo", http.StatusBadRequest)
+			http.Error(w, "Error al almacenar la imagen", http.StatusBadRequest)
 			file.Close()
-			tempFile.Close()
 			return
 		}
-		tempFile.Write(fileBytes)
 		file.Close()
-		tempFile.Close()
-		fmt.Fprintf(w, "Subida Completada '%s'\n",tempFile.Name())
+		fmt.Fprintf(w, "Enrrolado Exitosamente '%s'\n",identificador)
+	})
+	http.HandleFunc("/identificar", func(w http.ResponseWriter, r *http.Request) { 
+		fmt.Fprintf(w, "%s\n","Ricardo")
+		//http.Error(w, "Error al identificar", http.StatusBadRequest)
 	})
 	http.ListenAndServe(":5000", nil)
 }

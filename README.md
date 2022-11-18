b36903adff3afa16225627f8411d0e09906f3c4b
RicardoValladares <r_a_v_r_@hotmail.com>
2022-11-18 17:28:53 -0600

    Renombrado & html dentro de Go

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-# Detectar rostros
-Servidor Web Hecho en Golang para detectar y reconocer rostros
+# Rostro Biométrico
+Este proyecto es un servidor web hecho para la detección y reconocimiento de rostros; implementando un conjunto de herramientas javascript y golang.
 
-Librerias usadas:
+Librerías usadas:
 - Go-Face (https://github.com/Kagami/go-face)
 - Go-Recognizer (https://github.com/leandroveronezi/go-recognizer)
-- opencv.js (https://docs.opencv.org/3.4/d5/d10/tutorial_js_root.html)
+- OpenCV (https://docs.opencv.org/3.4/d5/d10/tutorial_js_root.html)
 
+Modelos entrenados para el reconocimiento, detección y predicción de rostros:
+- @davisking (https://github.com/davisking/dlib-models)
 
 ![Captura de Pantalla](https://raw.githubusercontent.com/RicardoValladares/FaceDetect/facedetection/captura.png)
+
+## Reconocimiento - dlib_face_recognition_resnet_model_v1.dat
+La red se entrenó desde cero con un conjunto de datos de aproximadamente 3 millones de rostros. Este conjunto de datos se deriva de dos conjuntos de datos. El conjunto de datos de face scrub (http://vintage.winklerbros.net/facescrub.html) y el conjunto de datos de VGG (http://www.robots.ox.ac.uk/~vgg/data/vgg_face/). Se hizo este modelo entrenando una CNN de reconocimiento facial y luego usando métodos de agrupación de gráficos y mucha revisión manual para limpiar el conjunto de datos. Al final, aproximadamente la mitad de las imágenes son de VGG y face scrub. Además, el número total de identidades individuales en el conjunto de datos es 7485. Se evitaron superposiciones de identidades en LFW.
+
+## Detección - mmod_human_face_detector.dat
+Este modelo está entrenado con el conjunto de datos: http://dlib.net/files/data/dlib_face_detection_dataset-2016-09-30.tar.gz. Se creó el conjunto de datos encontrando imágenes de rostros en muchos conjuntos de datos de imágenes disponibles públicamente (excluyendo el conjunto de datos FDDB). En particular, hay imágenes de ImageNet, AFLW, Pascal VOC, el conjunto de datos VGG, WIDER y face scrub. Todas las anotaciones en el conjunto de datos fueron creadas usando la herramienta imglab de dlib.
+
+## Predicción - shape_predictor_5_face_landmarks.dat
+Este es un modelo de referencia de 5 puntos que identifica las esquinas de los ojos y la parte inferior de la nariz. Se entrenó con el conjunto de datos que se encuentra en http://dlib.net/files/data/dlib_faces_5points.tar, que consta de 7198 rostros. Se creó este conjunto de datos descargando imágenes de Internet y anotándolas con la herramienta imglab de dlib. Este modelo está diseñado para funcionar bien con el detector de rostros HOG de dlib y el detector de rostros CNN (mmod_human_face_detector.dat).
--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"log"
 	"os"
 	"io"
 	"io/ioutil"
-
-	"log"
-	"github.com/leandroveronezi/go-recognizer"
 	"path/filepath"
+	"github.com/leandroveronezi/go-recognizer"
 )
 
 
 const fotosDir = "enrrolados"
-const dataDir = "models"
+const dataDir = "modelos"
+var rec = recognizer.Recognizer{}
 
 func addFile(rec *recognizer.Recognizer, Path, Id string) {
 	err := rec.AddImageToDataset(Path, Id)
@@ -24,52 +24,309 @@ func addFile(rec *recognizer.Recognizer, Path, Id string) {
 	}
 }
 
-
-var rec = recognizer.Recognizer{}
-
 func main() {
-		err := rec.Init(dataDir)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		rec.Tolerance = 0.4
-		rec.UseGray = true
-		rec.UseCNN = false
-		defer rec.Close()
-
-
-		files, err := ioutil.ReadDir("./enrrolados/")
-	    if err != nil {
-	        log.Fatal(err)
-	    }
-	
-	    for _, file := range files {
-	    	addFile(&rec, filepath.Join(fotosDir, file.Name()), file.Name())
-	        fmt.Println(file.Name())
-	    }
-		
-		rec.SetSamples()
-
-		
-	
-
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/identificar.html")})
-	http.HandleFunc("/enrrolar.html", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/enrrolar.html")})
-	http.HandleFunc("/opencv.js", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/opencv.js")})
-	http.HandleFunc("/utils.js", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/utils.js")})
-	http.HandleFunc("/haarcascade_frontalface_default.xml", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/haarcascade_frontalface_default.xml")})
+	err := rec.Init(dataDir)
+	if err != nil {
+		log.Println("Error al inicializar biometria con los templates")
+		return
+	}
+	rec.Tolerance = 0.4
+	rec.UseGray = true
+	rec.UseCNN = false
+	defer rec.Close()
+	files, err := ioutil.ReadDir("./enrrolados/")
+	if err != nil {
+		log.Println("Error al inicializar biometria con los rostros enrrolados")
+		return
+	}
+	fmt.Println("Cargando rostros enrrolados...")
+	for _, file := range files {
+		addFile(&rec, filepath.Join(fotosDir, file.Name()), file.Name())
+		log.Println(file.Name())
+	}
+	rec.SetSamples()
+	fmt.Println("Cargando servidor...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { 
+		fmt.Fprintf(w, `<html>
+			<head>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0'> 
+				<title>Enrrolar</title>
+				<script async src='opencv.js' onload='openCvReady();'></script>
+				<script src='utils.js'></script>
+				<style> .labele { color: white; padding: 8px; font-family: Arial; background-color: #ff9800; } .labelt { color: white; padding: 8px; font-family: Arial; background-color: #04aa6d; } @media only screen and (max-width: 992px) { video.camara { height:640px; width:480px; display: block; margin-left: auto; margin-right: auto; } } @media only screen and (min-width: 993px) { video.camara { height:480px; width:640px; display: block; margin-left: auto; margin-right: auto;} } </style>
+			</head>
+			<body  bgcolor='#000' oncontextmenu='return false' onkeydown='return false' onload="setTimeout('temporizador()',1000)"> <br>
+				<center> 
+					<span id='Tiempo' class='labelt'>0</span> 
+					<span id='Estado' class='labele'>Iniciando...</span> 
+				</center><br><br>
+				<center>
+<! ---------------------------------------------------------- si quieres ver el recuadro, descomenta la siguiente linea ---------------------------------------------------------- >
+					<canvas id='canvas_output' /></canvas> 
+					<video id='cam_input' height='480' width='640' class='camara'></video> 
+				</center>
+				<script>
+					/* variables globales para el funcionamiento */
+					let tiempo = 0; //variable que sirve como contador de segundos
+					
+					/* aperturamos webcam con opencv */
+					function openCvReady() {
+						cv['onRuntimeInitialized'] = () => {
+							let video = document.getElementById('cam_input');
+// ---------------------------------------------------------- si quieres ver solo la imagen con recuadro, descomenta la siguiente linea ----------------------------------------------------------
+							video.style.display='none'; 
+							navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function (stream) { video.srcObject = stream; video.play(); }).catch(function (err) { console.log('Error: ' + err); });
+							let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+							let gray = new cv.Mat();
+							let cap = new cv.VideoCapture(cam_input);
+							let faces = new cv.RectVector();
+							let faceClassifier = new cv.CascadeClassifier();
+							let utils = new Utils('errorMessage');
+							let faceCascade = 'haarcascade_frontalface_default.xml';
+							utils.createFileFromUrl(faceCascade, faceCascade, () => { faceClassifier.load(faceCascade); });
+							const FPS = 40;
+							function processVideo() {
+								let begin = Date.now();
+								cap.read(src);
+								cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+								let detectado=0;
+								try {
+									faceClassifier.detectMultiScale(gray, faces, 1.1, 3, 0);
+									for (let i = 0; i < faces.size(); ++i) {
+										let face = faces.get(i);
+										let point1 = new cv.Point(face.x, face.y);
+										let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+										cv.rectangle(src, point1, point2, [0, 255, 0, 255]);
+										detectado = 1;
+									}
+								} catch (err) {
+									console.log(err);
+								}
+								if(detectado==1){
+									document.getElementById('Estado').innerHTML = 'Rostro detectado';
+									document.getElementById('Estado').style.backgroundColor='#04aa6d';
+								} else{
+									document.getElementById('Estado').innerHTML = 'Rostro no detectado';
+									document.getElementById('Estado').style.backgroundColor='#f44336';
+								}
+// ---------------------------------------------------------- si quieres ver la imagen con recuadro, descomenta la siguiente linea ----------------------------------------------------------
+								cv.imshow('canvas_output', src);
+								let delay = 1000 / FPS - (Date.now() - begin);
+								setTimeout(processVideo, delay);
+							} 
+							setTimeout(processVideo, 0);
+						}
+					}
+					
+					/* temporizador que usa la variable global tiempo para contar los segundos */
+					function temporizador() {
+						if(document.getElementById('Estado').textContent=='Rostro detectado'){
+							tiempo = tiempo + 1;
+							document.getElementById('Tiempo').innerHTML = tiempo;
+						} else{
+							tiempo = 0; //reiniciamos el contador si no detectamos rostro
+							document.getElementById('Tiempo').innerHTML = tiempo;
+						}
+						//cuando haya pasado 5 segundos de la deteccion de un rostro ejecutar()
+						if(tiempo == 5){
+							ejecutar(); 
+						}
+						setTimeout('temporizador()',1000);
+					}
+					
+					/* generamos el archivo de imagen sin el recuadro */
+					function ejecutar(){
+						let imageCanvas = document.createElement('canvas');
+						let imageCtx = imageCanvas.getContext('2d');
+						let v = document.getElementById('cam_input');
+						imageCanvas.width = v.videoWidth;
+						imageCanvas.height = v.videoHeight;
+						imageCtx.drawImage(v, 0, 0, v.videoWidth, v.videoHeight);
+						imageCanvas.toBlob(postFile, 'image/jpeg');
+						tiempo = 0;
+					}
+					
+					/* enviamos el 'file' y el 'identificador' a la url 'enrrolar' por metodo 'POST' */
+					function postFile(file) {
+						let formdata = new FormData();
+						let identificador = prompt('Ingrese un identificador:');
+						if(identificador==null){
+							return
+						}
+						formdata.append('id', identificador);
+						formdata.append('image', file);
+						let xhr = new XMLHttpRequest();
+						xhr.open('POST', 'enrrolar', true);
+						xhr.onload = function () {
+							if (this.status === 200)
+								alert(this.response); //si se hizo un envio exitoso, sin error; mostramos la respuesta
+							else
+								alert(xhr); //si se hizo un envio con errores; mostramos el error
+						};
+						xhr.send(formdata);
+					}
+				</script>
+			</body>
+		</html>`)
+	})
+	log.Println("(http://localhost:5000) enrrolador biometrico")
+	http.HandleFunc("/identificar.html", func(w http.ResponseWriter, r *http.Request) { 
+		fmt.Fprintf(w, `<html>
+			<head>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0'> 
+				<title>Identificar</title>
+				<script async src='opencv.js' onload='openCvReady();'></script>
+				<script src='utils.js'></script>
+				<style> .labele { color: white; padding: 8px; font-family: Arial; background-color: #ff9800; } .labelt { color: white; padding: 8px; font-family: Arial; background-color: #04aa6d; } .labeli { color: white; padding: 8px; font-family: Arial; background-color: #f44336; } @media only screen and (max-width: 992px) { video.camara { height:640px; width:480px; display: block; margin-left: auto; margin-right: auto; } } @media only screen and (min-width: 993px) { video.camara { height:480px; width:640px; display: block; margin-left: auto; margin-right: auto;} } </style>
+			</head>
+			<body  bgcolor='#000' oncontextmenu='return false' onkeydown='return false' onload="setTimeout('temporizador()',1000)"> <br>
+				<center> 
+					<span id='Tiempo' class='labelt'>0</span> <span id='Estado' class='labele'>Iniciando...</span> 
+					<span id='Identificador' class='labeli'>Desconocido</span> 
+				</center><br><br>
+				<center>
+<! ---------------------------------------------------------- si quieres ver el recuadro, descomenta la siguiente linea ---------------------------------------------------------- >
+					<canvas id='canvas_output' /></canvas> 
+					<video id='cam_input' height='480' width='640' class='camara'></video> 
+				</center>
+				<script>
+					/* variables globales para el funcionamiento */
+					let identificado = 0; //variable que determina si hemos identificado a la persona
+					let tiempo = 0; //variable que sirve como contador de segundos
+					
+					/* aperturamos webcam con opencv */
+					function openCvReady() {
+						cv['onRuntimeInitialized'] = () => {
+							let video = document.getElementById('cam_input');
+// ---------------------------------------------------------- si quieres ver solo la imagen con recuadro, descomenta la siguiente linea ----------------------------------------------------------
+							video.style.display='none'; 
+							navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function (stream) { video.srcObject = stream; video.play(); }).catch(function (err) { console.log('Error: ' + err); });
+							let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+							let gray = new cv.Mat();
+							let cap = new cv.VideoCapture(cam_input);
+							let faces = new cv.RectVector();
+							let faceClassifier = new cv.CascadeClassifier();
+							let utils = new Utils('errorMessage');
+							let faceCascade = 'haarcascade_frontalface_default.xml';
+							utils.createFileFromUrl(faceCascade, faceCascade, () => { faceClassifier.load(faceCascade); });
+							const FPS = 40;
+							function processVideo() {
+								let begin = Date.now();
+								cap.read(src);
+								cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+								let detectado=0;
+								try {
+									faceClassifier.detectMultiScale(gray, faces, 1.1, 3, 0);
+									for (let i = 0; i < faces.size(); ++i) {
+										let face = faces.get(i);
+										let point1 = new cv.Point(face.x, face.y);
+										let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+										cv.rectangle(src, point1, point2, [0, 255, 0, 255]);
+										detectado = 1;
+									}
+								} catch (err) {
+									console.log(err);
+								}
+								if(detectado==1){
+									document.getElementById('Estado').innerHTML = 'Rostro detectado';
+									document.getElementById('Estado').style.backgroundColor='#04aa6d';
+								} else{
+									document.getElementById('Estado').innerHTML = 'Rostro no detectado';
+									document.getElementById('Estado').style.backgroundColor='#f44336';
+								}
+// ---------------------------------------------------------- si quieres ver la imagen con recuadro, descomenta la siguiente linea ----------------------------------------------------------
+								cv.imshow('canvas_output', src);
+								let delay = 1000 / FPS - (Date.now() - begin);
+								setTimeout(processVideo, delay);
+							} 
+							setTimeout(processVideo, 0);
+						}
+					}
+					
+					/* temporizador que usa la variable global tiempo para contar los segundos */
+					function temporizador() {
+						if(identificado==0){
+							if(document.getElementById('Estado').textContent=='Rostro detectado'){
+								tiempo = tiempo + 1;
+								document.getElementById('Tiempo').innerHTML = tiempo;
+							} else{
+								tiempo = 0; //reiniciamos contador si no detectamos rostro
+								document.getElementById('Tiempo').innerHTML = tiempo;
+							}
+							//cuando hayan pasado 5 segundos de la deteccion de un rostro ejecutar()
+							if(tiempo == 5){
+								ejecutar(); 
+							}
+						}
+						setTimeout('temporizador()',1000); //volver a ejecutar la funcion, dentro de un segundo
+					}
+					
+					/* generamos el archivo de imagen sin el recuadro */
+					function ejecutar(){
+						let imageCanvas = document.createElement('canvas');
+						let imageCtx = imageCanvas.getContext('2d');
+						let v = document.getElementById('cam_input');
+						imageCanvas.width = v.videoWidth;
+						imageCanvas.height = v.videoHeight;
+						imageCtx.drawImage(v, 0, 0, v.videoWidth, v.videoHeight);
+						imageCanvas.toBlob(postFile, 'image/jpeg');
+						tiempo = 0;
+					}
+					
+					/* enviamos el 'file' a la url 'identificar' por metodo 'POST' */
+					function postFile(file) {
+						let formdata = new FormData();
+						formdata.append('image', file);
+						let xhr = new XMLHttpRequest();
+						xhr.open('POST', 'identificar', true);
+						xhr.onload = function () {
+							//si se hizo un envio exitoso, sin error; enviamos la respuerta indentificacion()
+							if (this.status === 200){
+								if (this.response != "NO IDENTIFICADO"){ identificacion(this.response); }
+							}
+						};
+						xhr.send(formdata);
+					}
+					
+					/* mostramos el Identificador de la persona por 5 segundos */
+					async function identificacion(res) {
+						identificado = 1;
+						document.getElementById('Identificador').innerHTML = res;
+						document.getElementById('Identificador').style.backgroundColor='#04aa6d';
+						await sleep(5 * 1000); //hacemos una pausa de 5 segundos
+						document.getElementById('Identificador').innerHTML = 'Desconocido';
+						document.getElementById('Identificador').style.backgroundColor='#f44336';
+						identificado = 0;
+					}
+					
+					/* funcion que simula sleep */
+					function sleep(ms) {
+						return new Promise((resolve) => setTimeout(resolve, ms));
+					}
+				</script>
+			</body>
+		</html>`)
+	})
+	log.Println("(http://localhost:5000/identificar.html) identificador biometrico")
+	http.HandleFunc("/opencv.js", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "opencv.js")})
+	//log.Println("(http://localhost:5000/opencv.js) libreria de deteccion de rostros")
+	http.HandleFunc("/utils.js", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "utils.js")})
+	//log.Println("(http://localhost:5000/utils.js) componentes extras para la libreria opencv")
+	http.HandleFunc("/haarcascade_frontalface_default.xml", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "haarcascade_frontalface_default.xml")})
+	//log.Println("(http://localhost:5000/haarcascade_frontalface_default.xml) modelo de deteccion de caras")
 	http.Handle("/enrrolados/", http.StripPrefix("/enrrolados", (http.FileServer(http.Dir("./enrrolados"))) ))
+	log.Println("(http://localhost:5000/enrrolados/) rostros enrrolados")
 	http.HandleFunc("/enrrolar", func(w http.ResponseWriter, r *http.Request) { 
 		identificador := r.FormValue("id")
 		if identificador == "" || identificador == "null" {
 			http.Error(w, "Error al obtener el identificador", http.StatusBadRequest)
+			log.Println("Error al obtener el identificador")
 			return
 		}
 		file, _, err := r.FormFile("image")
 		if err != nil {
 			http.Error(w, "Error al obtener el imagen", http.StatusBadRequest)
+			log.Println("Error al obtener el imagen")
 			file.Close()
 			return
 		}
@@ -77,67 +334,73 @@ func main() {
 		dst, err := os.Create("./enrrolados/"+identificador)
 		if err != nil {
 			http.Error(w, "Error al enrrolar imagen", http.StatusBadRequest)
+			log.Println("Error al enrrolar imagen")
 			file.Close()
+			dst.Close()
 			return
 		}
 		defer dst.Close()
 		_, err = io.Copy(dst, file)
 		if err != nil {
 			http.Error(w, "Error al almacenar la imagen", http.StatusBadRequest)
+			log.Println("Error al almacenar la imagen")
 			file.Close()
+			dst.Close()
 			return
 		}
 		file.Close()
+		dst.Close()
+		addFile(&rec, filepath.Join(fotosDir, identificador), identificador)
+		rec.SetSamples()
 		fmt.Fprintf(w, "Enrrolado Exitosamente '%s'\n",identificador)
+		log.Println("Enrrolado Exitosamente ",identificador)
 	})
+	//log.Println("(http://localhost:5000/enrrolar) webservice de enrrolamiento")
 	http.HandleFunc("/identificar", func(w http.ResponseWriter, r *http.Request) { 
-
-		
-		//fmt.Fprintf(w, "%s\n","Ricardo")
-		//http.Error(w, "Error al identificar", http.StatusBadRequest)
-
 		file, handler, err := r.FormFile("image")
 		if err != nil {
 			http.Error(w, "Error al obtener la imagen", http.StatusBadRequest)
+			log.Println("Error al obtener la imagen")
+			file.Close()
 			return
 		}
 		defer file.Close()
 		tempFile, err := ioutil.TempFile("temp", "*"+filepath.Ext(handler.Filename))
 		if err != nil {
 			http.Error(w, "Error al nombrar la imagen", http.StatusBadRequest)
+			log.Println("Error al nombrar la imagen")
+			file.Close()
+			tempFile.Close()
 			return
 		}
 		defer tempFile.Close()
-		
 		fileBytes, err := ioutil.ReadAll(file)
 		if err != nil {
 			http.Error(w, "Error al leer la imagen", http.StatusBadRequest)
+			log.Println("Error al leer la imagen")
+			file.Close()
+			tempFile.Close()
 			return
 		}
 		tempFile.Write(fileBytes)
-			//fmt.Fprintf(w, "Subida Completada '%s'\n",)
-		
-
 		face, err := rec.ClassifyMultiples(tempFile.Name())
-		//_, err = rec.Classify(tempFile.Name())
 		if err != nil {
-	        log.Fatal(err)
-	        return
-	    }
-
-		if len(face) > 0 {
-			fmt.Println(face[0].Data.Id)
-			fmt.Fprintf(w, face[0].Data.Id)
-		} else {
-			fmt.Println("NO IDENTIFICADO")
-			fmt.Fprintf(w, "NO IDENTIFICADO")
+			http.Error(w, "Error al comparar rostro", http.StatusBadRequest)
+			log.Println("Error al comparar rostro")
+			file.Close()
+			tempFile.Close()
+			return
 		}
-		
-
-
-
-
-		
+		if len(face) > 0 {
+			fmt.Fprintf(w, face[0].Data.Id)
+			log.Println(face[0].Data.Id)
+		} else {
+			fmt.Fprintf(w, "NO IDENTIFICADO")
+			log.Println("NO IDENTIFICADO")
+		}
+		file.Close()
+		tempFile.Close()		
 	})
+	//log.Println("(http://localhost:5000/identificar) webservice de identificacion")
 	http.ListenAndServe(":5000", nil)
 }

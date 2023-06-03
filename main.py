7f3b680caae6912cecd06beb3e05e7b0f6ebaccc
RicardoValladares <r_a_v_r_@hotmail.com>
2023-06-03 09:28:49 -0600

    Se incremento la tolerancia para ser menos estricto en el match

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def identificar():
         file.save('%s/%s' % ("./temp", nuevonombre))
         face_image = face_recognition.load_image_file("./temp" + '/' + nuevonombre)
         unk_face_encoding = face_recognition.face_encodings(face_image)[0]
-        results = face_recognition.compare_faces(enrrolados_faces, unk_face_encoding, tolerance=0.4)
+        results = face_recognition.compare_faces(enrrolados_faces, unk_face_encoding, tolerance=0.6)
         return Response(res[results.index(True)])
     except:
         return Response("NO IDENTIFICADO")
